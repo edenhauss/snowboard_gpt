a57e298aa0f4d8c6b0eddb74f3fe8427006bc7e0
merchfck <logistikman@bk.ru>
2024-02-26 00:27:46 +0300

    libs.py

--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,4 @@
-from config import api_key
-from typing import Dict, Tuple, Any, Generator
-from openai import Client, BadRequestError, PermissionDeniedError, AuthenticationError
-from openai.types.chat.chat_completion import ChatCompletion
+from libs import *
 
 class UserInfo:
     def __init__(self) -> None:
