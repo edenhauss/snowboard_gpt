2b8c28fe3b4930e3287a1ad7c6b24ddafefc064f
merchfck <logistikman@bk.ru>
2024-02-26 00:33:09 +0300

    remove libs.py

--- libs.py
+++
@@ -1,4 +0,0 @@
-from config import api_key
-from typing import Dict, Tuple, Any, Generator
-from openai import Client, BadRequestError, PermissionDeniedError, AuthenticationError
-from openai.types.chat.chat_completion import ChatCompletion
